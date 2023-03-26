2ae8aaabfc0aceda6f64b3985c088bb91b2adc8e
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-26 14:21:57 -0500

    studf

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.royalschool.foropec',
+  appName: 'ForoPEC',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
