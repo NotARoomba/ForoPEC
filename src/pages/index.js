fb7137559de68985e4104109d9265fe430617bee
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-29 22:04:45 -0500

    fotos y cosas

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,15 @@ export default function Home() {
     sizes="100vw"
     style={{ width: '100%', height: 'auto', paddingTop: '5px'}}/>
         {/* MAIN PAGE, INSERT PHOTOS AND  TEXT ABOUT EH FORO*/}
+        <div className={styles.scrolling}>
+  <h1>Presentantes</h1>
+  <Image src="/person1.jpg" alt='person1' width={100} height={100} className={styles.image}/>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum sit amet enim a semper. Maecenas congue bibendum nibh, eget feugiat ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam malesuada purus sit amet metus tristique faucibus.</p>
+  <div className={styles.line}/>
+  <Image src="/person2.jpg" alt='person2' width={100} height={100} className={styles.image}/>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum sit amet enim a semper. Maecenas congue bibendum nibh, eget feugiat ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam malesuada purus sit amet metus tristique faucibus.</p>
+
+</div>
       </main>
     </>
   )
