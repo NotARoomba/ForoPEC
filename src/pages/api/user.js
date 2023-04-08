deea49cf723898bbba4b2ddce2f69e1f13a61a41
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-04-07 21:55:37 -0500

    uer

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -2,20 +2,40 @@ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-d
 dotenv.config()
 const { MongoClient } = require("mongodb");
 
-export default async function handler(req, res) {
-  const mongo = await MongoClient.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true,
-  })
-        const users = mongo.db("userData").collection("users");
-        users
-          .findOne(req.body)
-          .then((user) => {
-            res.send(user);
-          })
-          .catch((err) => {
-            res.send(err);
-          });
+/*
+EXAMPLE REQUEST
+
+{
+  body: {
+    email
   }
-  
+}
+
+-------------
+
+EXAMPLE USER DATA IN MONGO
+{
+  email
+  name
+  school
+  salon
+  hasFood
+}
+*/
+
+
+
+export default async function handler(req, res) {
+    const mongo = await MongoClient.connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        keepAlive: true,
+      })
+    const users = mongo.db("userData").collection("users");
+    const user = await users.findOne(req.body)
+    if (user != null) {
+      res.send(user)
+    } else {
+      res.sendStatus(400);
+    }
+}
