e1db8dc8429cf32c3dfc99c15dfa9a3dedacd079
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-28 19:20:47 -0500

    restart from scratch with react native

--- src/pages/schedule.js
+++
@@ -1,6 +0,0 @@
-import Head from 'next/head'
-export default function Schedule() {
-    return (
-      <div>INSERT Schedule PAGE HERE, USE API FROM API FOLDER</div>
-    )
-  }
--- src/pages/user.js
+++
@@ -1,6 +0,0 @@
-import Head from 'next/head'
-export default function User() {
-    return (
-      <div>INSERT PROFILE PAGE HERE, ADD A QR AS PROFILE PICTURE AND NAME AND EMAIL AND STUFF</div>
-    )
-  }
