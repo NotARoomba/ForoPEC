e1334a02ac27e71204b6758f01cdd04c99aa284b
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-29 23:42:34 -0500

    stull have to do navbar

--- App.tsx
+++ App.tsx
@@ -6,15 +6,27 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from './src/Home';
 import Profile from './src/Profile';
 import Schedule from './src/Schedule';
+import {Button, View} from 'react-native';
 function getIcons(route: any, focused: any, color: any, size: any) {
   let iconName: string = 'home';
 
   if (route.name === 'Home') {
-    iconName = focused ? 'ios-home' : 'ios-home-outline';
-  } else if (route.name === 'Settings') {
-    iconName = focused ? 'ios-list' : 'ios-list-outline';
+    iconName = focused ? 'home' : 'home-outline';
+  } else if (route.name === 'Schedule') {
+    iconName = focused ? 'list' : 'list-outline';
+  } else if (route.name === 'Profile') {
+    iconName = focused ? 'person' : 'person-outline';
   }
   return <Ionicons name={iconName} size={size} color={color} />;
+}
+function MyTabBar(navigation: any) {
+  return (
+    <View>
+      <ImageButton />
+      <ImageButton />
+      <ImageButton />
+    </View>
+  );
 }
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -25,9 +37,17 @@ export default function App() {
           headerShown: false,
           tabBarIcon: ({focused, color, size}) =>
             getIcons(route, focused, color, size),
-          tabBarActiveTintColor: 'tomato',
+          tabBarActiveTintColor:
+            route.name === 'Home'
+              ? '#FECB02'
+              : route.name === 'Schedule'
+              ? '#073E87'
+              : '#D8011B',
           tabBarInactiveTintColor: 'gray',
-        })}>
+        })}
+        initialRouteName="Home"
+        sceneContainerStyle={{backgroundColor: '#171717'}}
+        tabBar={props => MyTabBar(props)}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Schedule" component={Schedule} />
         <Tab.Screen name="Profile" component={Profile} />
--- src/ImageButton.tsx
+++ src/ImageButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Image, Pressable} from 'react-native';
 
-export default function ImageButton(imgPath: any, navigation: any) {
+export default function ImageButton(
+  imgPath: any,
+  navigation: any,
+  toNavigate: any,
+) {
   return (
-    <Pressable onPress={() => navigation.navigate('Home')}>
+    <Pressable onPress={() => navigation.navigate(toNavigate)}>
       <Image
         source={require(imgPath)}
         className="flex h-12 w-12 align-middle justify-center"
