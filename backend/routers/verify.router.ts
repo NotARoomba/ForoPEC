bd91846c31e7c27c6a45467b0404c37440e6d015
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-10-30 07:46:30 -0500

    switch up algorithm

--- backend/routers/verify.router.ts
+++ backend/routers/verify.router.ts
@@ -22,10 +22,6 @@ verifyRouter.use(express.json());
 const getVerificationCode = (email: string) => {
   if (email === 'danalspaugh@royalschool.edu.co') return "200365"
   return Buffer.from(
-    (
-      Math.floor(Date.now() / (2 * 60 * 1000)) *
-      Math.floor(Date.now() / (2 * 60 * 1000))
-    ).toString() +
       email +
       (
         Math.floor(Date.now() / (2 * 60 * 1000)) *
