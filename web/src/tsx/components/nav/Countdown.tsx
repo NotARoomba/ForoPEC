43bd91eb65fe2929c5eda30366af734679c1c5be
Nathan <70660308+NotARoomba@users.noreply.github.com>
2024-10-18 10:56:06 -0500

    stuff

--- web/src/tsx/components/nav/Countdown.tsx
+++ web/src/tsx/components/nav/Countdown.tsx
@@ -8,7 +8,7 @@ export default function Countdown({nav}: CountdownProps) {
   );
   useEffect(() => {
     const dateInterval = setInterval(() => {
-      setTimeRemaining(new Date('2024/10/20').getTime() - Date.now());
+      setTimeRemaining(new Date('2024/10/18').getTime() - Date.now());
     }, 1000);
     return () => clearInterval(dateInterval);
   }, []);
@@ -22,7 +22,7 @@ export default function Countdown({nav}: CountdownProps) {
           : // : ' flex lg:hidden mx-auto bg-gradient-to-br from-flag-yellow from-40% via-flag-blue via-50% to-flag-red to-60%  bg-clip-text text-transparent')
             ' flex lg:hidden mx-auto text-black font-sans')
       }>
-      <span className="text-2xl xl:text-2xl  my-auto mx-2">
+      {Math.floor(timeRemaining / (1000 * 60 * 60 * 24)) >= 0 ? <span className="text-2xl xl:text-2xl  my-auto mx-2">
         {('0' + Math.floor(timeRemaining / (1000 * 60 * 60 * 24))).slice(
           Math.floor(timeRemaining / (1000 * 60 * 60 * 24)).toString().length >
             2
@@ -32,7 +32,7 @@ export default function Countdown({nav}: CountdownProps) {
         : {('0' + Math.floor(timeRemaining / (1000 * 60 * 60))).slice(-2)} :{' '}
         {('0' + Math.floor(timeRemaining / (1000 * 60))).slice(-2)} :{' '}
         {('0' + Math.floor(timeRemaining / 1000)).slice(-2)}
-      </span>
+      </span> : '00 : 00 : 00 : 00'}
       {/* <CountdownTime
         remaining={Math.floor(timeRemaining / (1000 * 60 * 60 * 24))}
         unit={'Dias'}
