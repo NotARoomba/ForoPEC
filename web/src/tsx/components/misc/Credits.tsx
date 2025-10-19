a4584aa1944960363e424cdbec10e97ae4a8ce4a
Kae-72 <ouverdraiva@gmail.com>
2025-10-19 17:57:08 -0500

    Update Credits.tsx

--- web/src/tsx/components/misc/Credits.tsx
+++ web/src/tsx/components/misc/Credits.tsx
@@ -1,7 +1,7 @@
 export default function Credits() {
   return (
     <div className="bg-zircon text-2 bg-opacity-50 opacity-50 px-2 py-1 fixed rounded w-fit bottom-1 left-1">
-      made by @notaroomba and updated by Nairon Barrios
+      made by @notaroomba and adapted by Nairon Barrios
     </div>
   );
 }
